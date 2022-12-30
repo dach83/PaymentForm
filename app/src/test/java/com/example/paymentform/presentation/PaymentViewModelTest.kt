5b842893f0f84da50762640c06dd4b892dbf8a93
dach83 <dach83@mail.ru>
2022-12-30 16:08:09 +0500

    Fail test incorrect_card_number

--- app/src/test/java/com/example/paymentform/presentation/PaymentViewModelTest.kt
+++ app/src/test/java/com/example/paymentform/presentation/PaymentViewModelTest.kt
@@ -7,7 +7,7 @@ import org.junit.Test
 class PaymentViewModelTest {
 
     @Test
-    fun zero_sum_payment() {
+    fun zero_payment_sum() {
         // arrange
         val sut = PaymentViewModel()
         sut.onEvent(PaymentEvent.ChangeSum("0"))
@@ -18,5 +18,33 @@ class PaymentViewModelTest {
         // assert
         assertEquals(false, sut.uiState.submitted)
         assertEquals(R.string.enter_a_positive_non_zero_number, sut.uiState.sumError)
+    }
+
+    @Test
+    fun negative_payment_sum() {
+        // arrange
+        val sut = PaymentViewModel()
+        sut.onEvent(PaymentEvent.ChangeSum("-100"))
+
+        // act
+        sut.onEvent(PaymentEvent.Submit)
+
+        // assert
+        assertEquals(false, sut.uiState.submitted)
+        assertEquals(R.string.enter_a_positive_non_zero_number, sut.uiState.sumError)
+    }
+
+    @Test
+    fun incorrect_card_number() {
+        // arrange
+        val sut = PaymentViewModel()
+        sut.onEvent(PaymentEvent.ChangeCardNumber("0001 322 4534 2333"))
+
+        // act
+        sut.onEvent(PaymentEvent.Submit)
+
+        // assert
+        assertEquals(false, sut.uiState.submitted)
+        assertEquals(R.string.incorrect_card_number, sut.uiState.sumError)
     }
 }
