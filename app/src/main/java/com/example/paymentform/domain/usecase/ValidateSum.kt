6200fe93b566aef3507ab2d0c24219c541cc0b65
dach83 <dach83@mail.ru>
2022-12-30 16:35:38 +0500

    Test too_much_payment_sum passes

--- app/src/main/java/com/example/paymentform/domain/usecase/ValidateSum.kt
+++ app/src/main/java/com/example/paymentform/domain/usecase/ValidateSum.kt
@@ -5,13 +5,22 @@ import com.example.paymentform.R
 class ValidateSum {
     operator fun invoke(sum: String): ValidateResult {
         val value = sum.toDoubleOrNull() ?: 0.0
-        if (value <= 0.01) {
+        if (value < 0.01) {
             return ValidateResult(
                 valid = false,
                 error = R.string.enter_a_positive_non_zero_number
             )
         }
-
+        if (value > MAX_SUM) {
+            return ValidateResult(
+                valid = false,
+                error = R.string.payment_amount_is_too_large
+            )
+        }
         return ValidateResult(valid = true)
+    }
+
+    companion object {
+        private const val MAX_SUM = 1_000_000
     }
 }
